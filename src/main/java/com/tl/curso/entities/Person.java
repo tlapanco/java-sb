12badd47537aa73d3139fba5faeecc2db8a1967a
tl <99222342+tlapanco@users.noreply.github.com>
2025-08-22 20:24:36 -0600

    Fix - Lombok annotations

--- src/main/java/com/tl/curso/entities/Person.java
+++ src/main/java/com/tl/curso/entities/Person.java
@@ -9,12 +9,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
  * @author tl
  */
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "people")
 public class Person {
     
